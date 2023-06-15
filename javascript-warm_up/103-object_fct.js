d685eef760a7cafb61920264b0f775ff428841ea
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2023-06-15 10:46:12 -0700

    Commiting 103-object_fct.js

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -2,15 +2,16 @@
 const myObject = {
   type: 'object',
   value: 12,
-  incr: function () {
+  incr: function() {
     this.value++;
   }
 };
+
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
 console.log(myObject);
 
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
