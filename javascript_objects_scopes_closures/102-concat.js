5c8858e2152116e525b8597d009e874d4471aa73
Maxime-Bakunzi <m.bakunzi@alustudent.com>
2023-06-16 13:38:12 -0700

    Committing

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -1,15 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-const sourceFile1 = process.argv[2];
-const sourceFile2 = process.argv[3];
-const destinationFile = process.argv[4];
-
-const content1 = fs.readFileSync(sourceFile1, 'utf8');
-const content2 = fs.readFileSync(sourceFile2, 'utf8');
-const concatenatedContent = content1 + content2;
-
-fs.writeFileSync(destinationFile, concatenatedContent);
-
-console.log(`Files '${sourceFile1}' and '${sourceFile2}' successfully concatenated to '${destinationFile}'.`);
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);
--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
+    if ((w <= 0) || (h <= 0) || !h || !w) {
+      return this;
     }
-
     this.width = w;
     this.height = h;
   }
-}
-
-module.exports = Rectangle;
+};
